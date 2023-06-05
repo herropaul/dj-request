514b2b722ada8188839a7e8bbc8e530bb2e4602c
Paul Nguon <herropaul@gmail.com>
2023-06-04 19:09:41 -0700

    testing api end points

---
+++ pages/api/users.ts
@@ -0,0 +1,13 @@
+// pages/api/users.ts
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    // Send 'John Doe' as response
+    res.status(200).json({ name: "John Doe" });
+  } else {
+    // Handle any other HTTP methods
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
