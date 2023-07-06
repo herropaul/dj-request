15410008b791b034b2313be1f8c95323b42b36df
Paul Nguon <herropaul@gmail.com>
2023-07-06 11:42:06 -0700

    added chakra provider context as workaround for next13

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import QrCode from "../components/QrCode";
 
 export default function Home() {
   return (
-    <div>
+    <div className=" flex flex-col justify-center items-center h-screen ">
       <h1>Welcome to Users Homepage</h1>
       <label>Enter song: </label>
       <input className=" border-black" type="text" id="song"></input>
-      <QrCode />
+      {/* <QrCode /> */}
     </div>
   );
 }
